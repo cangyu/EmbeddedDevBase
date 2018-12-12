96638cfbfe44fd457e40704905f0660c8f35b6be
Yu Cang <yhcy1993@gmail.com>
2018-12-12 14:31:51 +0800

    fatfs on sd card test ok

--- APP/main.cpp
+++ APP/main.cpp
@@ -1,17 +1,16 @@
 #include "bsp.h"
+#include <stdio.h>
 
 FATFS fs;
 FIL file;
+DIR dir;
 FRESULT f_res;
 UINT fnum;
-BYTE ReadBuffer[1024]={0};
-BYTE WriteBuffer[] = {"LLF RC LOG.\n"};
+char WrtBuf[1024];
 
-void main()
+void BSP_SD_Init()
 {
-    BSP_Init();
-    
-    printf("Testing FATFS over SD card...\n");
+    //Mount
 	f_res = f_mount(&fs,"0:",1);
 	if(f_res == FR_NO_FILESYSTEM)
 	{
@@ -43,8 +42,74 @@ void main()
     else
         printf("SD card mounterd!\n");
     
+    //Create folder
+    printf("Trying open the log folder...\n");
+    f_res=f_opendir(&dir,"0:LLF_RC_LOG");
+    if(f_res!=FR_OK)
+    {
+        printf("Log folder does not exist!\n");
+        f_res=f_mkdir("0:LLF_RC_LOG");
+        if(f_res != FR_OK)
+        {
+            printf("Failed to create folder!\n");
+            while(1);
+        }
+        else
+          printf("Folder created!\n");
+    }
+    else
+    {
+        printf("Log folder exists!\n");
+        
+        printf("Trying to close the log folder...\n");
+        f_res=f_closedir(&dir);
+        if(f_res != FR_OK)
+        {
+            printf("Failed to close folder!\n");
+            while(1);
+        }
+        else
+          printf("Folder closed!\n");
+    }
+    
+    //Create Log file
+	printf("Trying to create the log file...\n");	
+	f_res = f_open(&file, "0:LLF_RC_LOG/20181212.txt",FA_CREATE_ALWAYS | FA_WRITE);
+	if (f_res == FR_OK)
+	{   
+        printf("Log file opened!\n"); 
+        for(int i = 0; i<100; i++)
+        {
+            int cnt = sprintf(WrtBuf, "%d\n", i);
+            f_res = f_write(&file, WrtBuf, cnt, &fnum);
+            if(f_res == FR_OK)
+            {
+                printf("%d write ok!\n", i);
+            }
+            else
+                printf("%d write failed!\n", i);
+        }
+        f_res = f_close(&file);
+        if(f_res == FR_OK)
+          printf("log file closed!\n");
+        else
+          printf("Failed to close the log file!\n");
+	}
+	else
+	{	
+		printf("Failed to create the log file!\n");
+	}
+    
+    //Unmount
 	f_mount(NULL,"0:",1);
-      
+}
+
+void main()
+{
+    BSP_Init();
+    
+    BSP_SD_Init();
+    
     while(1)
     {
         BSP_LED_Toggle(0);
